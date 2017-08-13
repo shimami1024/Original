de6fdd72b5aea1dff535da83630bf52362816454
shimami1024 <miki-jg14booof@docomo.ne.jp>
2017-08-13 15:48:01 +0900

    レイアウト9

---
+++ app/src/main/java/com/example/lit/original/Make9Activity.java
@@ -0,0 +1,124 @@
+package com.example.lit.original;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+
+public class Make9Activity extends AppCompatActivity {
+    private static final int REQUEST_GALLERY = 0;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+
+    SharedPreferences preferences;
+
+    /** Called when the activity is first created. */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make9);
+
+        imageView1 = (ImageView)findViewById(R.id.imageView1);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
+        imageView3 = (ImageView)findViewById(R.id.imageView3);
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        editText3 = (EditText)findViewById(R.id.editText3);
+
+        preferences = getSharedPreferences("pref_memo", MODE_PRIVATE);
+
+        editText1.setText(preferences.getString("memo1",""));
+        editText2.setText(preferences.getString("memo2",""));
+        editText3.setText(preferences.getString("memo3",""));
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ギャラリー呼び出し
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+
+            }});
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ギャラリー呼び出し
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 2);
+
+            }});
+
+        imageView3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // ギャラリー呼び出し
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 3);
+
+            }});
+    }
+
+    public void save(View v){
+        String memo1Text = editText1.getText().toString();
+        String memo2Text = editText2.getText().toString();
+        String memo3Text = editText3.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("memo1",memo1Text);
+        editor.putString("memo2",memo2Text);
+        editor.putString("memo3",memo3Text);
+        editor.commit();
+
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        // TODO Auto-generated method stub
+
+        if(resultCode == RESULT_OK) {
+            //ギャラリーから画像を選択してくる
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                Bitmap img = BitmapFactory.decodeStream(in);
+                in.close();
+                Log.d("あああああああああ", "いいいいいいいいい");
+                // 選択した画像を表示
+                if (requestCode == 1){
+                    imageView1.setImageBitmap(img);
+                } else if(requestCode == 2){
+                    imageView2.setImageBitmap(img);
+                } else if(requestCode == 3){
+                    imageView3.setImageBitmap(img);
+                }
+
+            } catch (Exception e) {
+                System.out.println("test");
+            }
+        }
+    }
+
+
+
+}
