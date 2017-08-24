71ad4a5e05ba2415d3ba6e09b796aff2ba05f9c5
shimami1024 <miki-jg14booof@docomo.ne.jp>
2017-08-24 18:12:30 +0900

    背景設定機能完了

---
+++ app/src/main/java/com/example/lit/original/BackGroundC1Activity.java
@@ -0,0 +1,459 @@
+package com.example.lit.original;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BackGroundC1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_ground_c1);
+    }
+
+    public void c1(View v) {
+        Intent intentMM1 = getIntent();
+        int make1 = intentMM1.getIntExtra("M1", 0);
+
+        Intent intentMM2 = getIntent();
+        int make2 = intentMM2.getIntExtra("M2", 0);
+
+        Intent intentMM3 = getIntent();
+        int make3 = intentMM3.getIntExtra("M3", 0);
+
+        Intent intentMM4 = getIntent();
+        int make4 = intentMM4.getIntExtra("M4", 0);
+
+        Intent intentMM5 = getIntent();
+        int make5 = intentMM5.getIntExtra("M5", 0);
+
+        Intent intentMM6 = getIntent();
+        int make6 = intentMM6.getIntExtra("M6", 0);
+
+        Intent intentMM7 = getIntent();
+        int make7 = intentMM7.getIntExtra("M7", 0);
+
+        Intent intentMM8 = getIntent();
+        int make8 = intentMM8.getIntExtra("M8", 0);
+
+        Intent intentMM9 = getIntent();
+        int make9 = intentMM9.getIntExtra("M9", 0);
+
+        Intent intentMM10 = getIntent();
+        int make10 = intentMM10.getIntExtra("M10", 0);
+
+        if (make1 == 1) {
+            Intent intent241 = new Intent(this, Make1Activity.class);
+            intent241.putExtra("background241", 241);
+            startActivity(intent241);
+        } else if (make2 == 2) {
+            Intent intent242 = new Intent(this, Make2Activity.class);
+            intent242.putExtra("background242", 242);
+            startActivity(intent242);
+        } else if (make3 == 3) {
+            Intent intent243 = new Intent(this, Make3Activity.class);
+            intent243.putExtra("background243", 243);
+            startActivity(intent243);
+        } else if (make4 == 4) {
+            Intent intent244 = new Intent(this, Make4Activity.class);
+            intent244.putExtra("background244", 244);
+            startActivity(intent244);
+        } else if (make5 == 5) {
+            Intent intent245 = new Intent(this, Make5Activity.class);
+            intent245.putExtra("background245", 245);
+            startActivity(intent245);
+        } else if (make6 == 6) {
+            Intent intent246 = new Intent(this, Make6Activity.class);
+            intent246.putExtra("background246", 246);
+            startActivity(intent246);
+        } else if (make7 == 7) {
+            Intent intent247 = new Intent(this, Make7Activity.class);
+            intent247.putExtra("background247", 247);
+            startActivity(intent247);
+        } else if (make8 == 8) {
+            Intent intent248 = new Intent(this, Make8Activity.class);
+            intent248.putExtra("background248", 248);
+            startActivity(intent248);
+        } else if (make9 == 9) {
+            Intent intent249 = new Intent(this, Make9Activity.class);
+            intent249.putExtra("background249", 249);
+            startActivity(intent249);
+        } else if (make10 == 10) {
+            Intent intent250 = new Intent(this, Make10Activity.class);
+            intent250.putExtra("background250", 250);
+            startActivity(intent250);
+        }
+    }
+
+    public void c2(View v) {
+        Intent intentMM1 = getIntent();
+        int make1 = intentMM1.getIntExtra("M1", 0);
+
+        Intent intentMM2 = getIntent();
+        int make2 = intentMM2.getIntExtra("M2", 0);
+
+        Intent intentMM3 = getIntent();
+        int make3 = intentMM3.getIntExtra("M3", 0);
+
+        Intent intentMM4 = getIntent();
+        int make4 = intentMM4.getIntExtra("M4", 0);
+
+        Intent intentMM5 = getIntent();
+        int make5 = intentMM5.getIntExtra("M5", 0);
+
+        Intent intentMM6 = getIntent();
+        int make6 = intentMM6.getIntExtra("M6", 0);
+
+        Intent intentMM7 = getIntent();
+        int make7 = intentMM7.getIntExtra("M7", 0);
+
+        Intent intentMM8 = getIntent();
+        int make8 = intentMM8.getIntExtra("M8", 0);
+
+        Intent intentMM9 = getIntent();
+        int make9 = intentMM9.getIntExtra("M9", 0);
+
+        Intent intentMM10 = getIntent();
+        int make10 = intentMM10.getIntExtra("M10", 0);
+
+        if (make1 == 1) {
+            Intent intent251 = new Intent(this, Make1Activity.class);
+            intent251.putExtra("background251", 251);
+            startActivity(intent251);
+        } else if (make2 == 2) {
+            Intent intent252 = new Intent(this, Make2Activity.class);
+            intent252.putExtra("background252", 252);
+            startActivity(intent252);
+        } else if (make3 == 3) {
+            Intent intent253 = new Intent(this, Make3Activity.class);
+            intent253.putExtra("background253", 253);
+            startActivity(intent253);
+        } else if (make4 == 4) {
+            Intent intent254 = new Intent(this, Make4Activity.class);
+            intent254.putExtra("background254", 254);
+            startActivity(intent254);
+        } else if (make5 == 5) {
+            Intent intent255 = new Intent(this, Make5Activity.class);
+            intent255.putExtra("background255", 255);
+            startActivity(intent255);
+        } else if (make6 == 6) {
+            Intent intent256 = new Intent(this, Make6Activity.class);
+            intent256.putExtra("background256", 256);
+            startActivity(intent256);
+        } else if (make7 == 7) {
+            Intent intent257 = new Intent(this, Make7Activity.class);
+            intent257.putExtra("background257", 257);
+            startActivity(intent257);
+        } else if (make8 == 8) {
+            Intent intent258 = new Intent(this, Make8Activity.class);
+            intent258.putExtra("background258", 258);
+            startActivity(intent258);
+        } else if (make9 == 9) {
+            Intent intent259 = new Intent(this, Make9Activity.class);
+            intent259.putExtra("background259", 259);
+            startActivity(intent259);
+        } else if (make10 == 10) {
+            Intent intent260 = new Intent(this, Make10Activity.class);
+            intent260.putExtra("background260", 260);
+            startActivity(intent260);
+        }
+    }
+
+    public void c3(View v) {
+        Intent intentMM1 = getIntent();
+        int make1 = intentMM1.getIntExtra("M1", 0);
+
+        Intent intentMM2 = getIntent();
+        int make2 = intentMM2.getIntExtra("M2", 0);
+
+        Intent intentMM3 = getIntent();
+        int make3 = intentMM3.getIntExtra("M3", 0);
+
+        Intent intentMM4 = getIntent();
+        int make4 = intentMM4.getIntExtra("M4", 0);
+
+        Intent intentMM5 = getIntent();
+        int make5 = intentMM5.getIntExtra("M5", 0);
+
+        Intent intentMM6 = getIntent();
+        int make6 = intentMM6.getIntExtra("M6", 0);
+
+        Intent intentMM7 = getIntent();
+        int make7 = intentMM7.getIntExtra("M7", 0);
+
+        Intent intentMM8 = getIntent();
+        int make8 = intentMM8.getIntExtra("M8", 0);
+
+        Intent intentMM9 = getIntent();
+        int make9 = intentMM9.getIntExtra("M9", 0);
+
+        Intent intentMM10 = getIntent();
+        int make10 = intentMM10.getIntExtra("M10", 0);
+
+        if (make1 == 1) {
+            Intent intent261 = new Intent(this, Make1Activity.class);
+            intent261.putExtra("background261", 261);
+            startActivity(intent261);
+        } else if (make2 == 2) {
+            Intent intent262 = new Intent(this, Make2Activity.class);
+            intent262.putExtra("background262", 262);
+            startActivity(intent262);
+        } else if (make3 == 3) {
+            Intent intent263 = new Intent(this, Make3Activity.class);
+            intent263.putExtra("background263", 263);
+            startActivity(intent263);
+        } else if (make4 == 4) {
+            Intent intent264 = new Intent(this, Make4Activity.class);
+            intent264.putExtra("background264", 264);
+            startActivity(intent264);
+        } else if (make5 == 5) {
+            Intent intent265 = new Intent(this, Make5Activity.class);
+            intent265.putExtra("background265", 265);
+            startActivity(intent265);
+        } else if (make6 == 6) {
+            Intent intent266 = new Intent(this, Make6Activity.class);
+            intent266.putExtra("background266", 266);
+            startActivity(intent266);
+        } else if (make7 == 7) {
+            Intent intent267 = new Intent(this, Make7Activity.class);
+            intent267.putExtra("background267", 267);
+            startActivity(intent267);
+        } else if (make8 == 8) {
+            Intent intent268 = new Intent(this, Make8Activity.class);
+            intent268.putExtra("background268", 268);
+            startActivity(intent268);
+        } else if (make9 == 9) {
+            Intent intent269 = new Intent(this, Make9Activity.class);
+            intent269.putExtra("background269", 269);
+            startActivity(intent269);
+        } else if (make10 == 10) {
+            Intent intent270 = new Intent(this, Make10Activity.class);
+            intent270.putExtra("background270", 270);
+            startActivity(intent270);
+        }
+    }
+
+    public void c4(View v) {
+        Intent intentMM1 = getIntent();
+        int make1 = intentMM1.getIntExtra("M1", 0);
+
+        Intent intentMM2 = getIntent();
+        int make2 = intentMM2.getIntExtra("M2", 0);
+
+        Intent intentMM3 = getIntent();
+        int make3 = intentMM3.getIntExtra("M3", 0);
+
+        Intent intentMM4 = getIntent();
+        int make4 = intentMM4.getIntExtra("M4", 0);
+
+        Intent intentMM5 = getIntent();
+        int make5 = intentMM5.getIntExtra("M5", 0);
+
+        Intent intentMM6 = getIntent();
+        int make6 = intentMM6.getIntExtra("M6", 0);
+
+        Intent intentMM7 = getIntent();
+        int make7 = intentMM7.getIntExtra("M7", 0);
+
+        Intent intentMM8 = getIntent();
+        int make8 = intentMM8.getIntExtra("M8", 0);
+
+        Intent intentMM9 = getIntent();
+        int make9 = intentMM9.getIntExtra("M9", 0);
+
+        Intent intentMM10 = getIntent();
+        int make10 = intentMM10.getIntExtra("M10", 0);
+
+        if (make1 == 1) {
+            Intent intent271 = new Intent(this, Make1Activity.class);
+            intent271.putExtra("background271", 271);
+            startActivity(intent271);
+        } else if (make2 == 2) {
+            Intent intent272 = new Intent(this, Make2Activity.class);
+            intent272.putExtra("background272", 272);
+            startActivity(intent272);
+        } else if (make3 == 3) {
+            Intent intent273 = new Intent(this, Make3Activity.class);
+            intent273.putExtra("background273", 273);
+            startActivity(intent273);
+        } else if (make4 == 4) {
+            Intent intent274 = new Intent(this, Make4Activity.class);
+            intent274.putExtra("background274", 274);
+            startActivity(intent274);
+        } else if (make5 == 5) {
+            Intent intent275 = new Intent(this, Make5Activity.class);
+            intent275.putExtra("background275", 275);
+            startActivity(intent275);
+        } else if (make6 == 6) {
+            Intent intent276 = new Intent(this, Make6Activity.class);
+            intent276.putExtra("background276", 276);
+            startActivity(intent276);
+        } else if (make7 == 7) {
+            Intent intent277 = new Intent(this, Make7Activity.class);
+            intent277.putExtra("background277", 277);
+            startActivity(intent277);
+        } else if (make8 == 8) {
+            Intent intent278 = new Intent(this, Make8Activity.class);
+            intent278.putExtra("background278", 278);
+            startActivity(intent278);
+        } else if (make9 == 9) {
+            Intent intent279 = new Intent(this, Make9Activity.class);
+            intent279.putExtra("background279", 279);
+            startActivity(intent279);
+        } else if (make10 == 10) {
+            Intent intent280 = new Intent(this, Make10Activity.class);
+            intent280.putExtra("background280", 280);
+            startActivity(intent280);
+        }
+    }
+
+    public void c12(View v){
+        Intent intentM1 = getIntent();
+        int make1 = intentM1.getIntExtra("M1", 0);
+        Intent intentMM1 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM2 = getIntent();
+        int make2 = intentM2.getIntExtra("M2", 0);
+        Intent intentMM2 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM3 = getIntent();
+        int make3 = intentM3.getIntExtra("M3", 0);
+        Intent intentMM3 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM4 = getIntent();
+        int make4 = intentM4.getIntExtra("M4", 0);
+        Intent intentMM4 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM5 = getIntent();
+        int make5 = intentM5.getIntExtra("M5", 0);
+        Intent intentMM5 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM6 = getIntent();
+        int make6 = intentM6.getIntExtra("M6", 0);
+        Intent intentMM6 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM7 = getIntent();
+        int make7 = intentM7.getIntExtra("M7", 0);
+        Intent intentMM7 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM8 = getIntent();
+        int make8 = intentM8.getIntExtra("M8", 0);
+        Intent intentMM8 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM9 = getIntent();
+        int make9 = intentM9.getIntExtra("M9", 0);
+        Intent intentMM9 = new Intent(this, BackGroundC2Activity.class);
+
+        Intent intentM10 = getIntent();
+        int make10 = intentM10.getIntExtra("M10", 0);
+        Intent intentMM10 = new Intent(this, BackGroundC2Activity.class);
+
+        if (make1 == 1){
+            intentMM1.putExtra("M1", 1);
+            startActivity(intentMM1);
+        } else if (make2 == 2){
+            intentMM2.putExtra("M2", 2);
+            startActivity(intentMM2);
+        } else if (make3 == 3){
+            intentMM3.putExtra("M3", 3);
+            startActivity(intentMM3);
+        } else if (make4 == 4){
+            intentMM4.putExtra("M4", 4);
+            startActivity(intentMM4);
+        } else if (make5 == 5){
+            intentMM5.putExtra("M5", 5);
+            startActivity(intentMM5);
+        } else if (make6 == 6){
+            intentMM6.putExtra("M6", 6);
+            startActivity(intentMM6);
+        } else if (make7 == 7){
+            intentMM7.putExtra("M7", 7);
+            startActivity(intentMM7);
+        } else if (make8 == 8){
+            intentMM8.putExtra("M8", 8);
+            startActivity(intentMM8);
+        } else if (make9 == 9){
+            intentMM9.putExtra("M9", 9);
+            startActivity(intentMM2);
+        } else if (make10 == 10) {
+            intentMM10.putExtra("M10", 10);
+            startActivity(intentMM10);
+        }
+    }
+
+    public void c10(View v){
+        Intent intentM1 = getIntent();
+        int make1 = intentM1.getIntExtra("M1", 0);
+        Intent intentMM1 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM2 = getIntent();
+        int make2 = intentM2.getIntExtra("M2", 0);
+        Intent intentMM2 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM3 = getIntent();
+        int make3 = intentM3.getIntExtra("M3", 0);
+        Intent intentMM3 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM4 = getIntent();
+        int make4 = intentM4.getIntExtra("M4", 0);
+        Intent intentMM4 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM5 = getIntent();
+        int make5 = intentM5.getIntExtra("M5", 0);
+        Intent intentMM5 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM6 = getIntent();
+        int make6 = intentM6.getIntExtra("M6", 0);
+        Intent intentMM6 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM7 = getIntent();
+        int make7 = intentM7.getIntExtra("M7", 0);
+        Intent intentMM7 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM8 = getIntent();
+        int make8 = intentM8.getIntExtra("M8", 0);
+        Intent intentMM8 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM9 = getIntent();
+        int make9 = intentM9.getIntExtra("M9", 0);
+        Intent intentMM9 = new Intent(this, BackGround0Activity.class);
+
+        Intent intentM10 = getIntent();
+        int make10 = intentM10.getIntExtra("M10", 0);
+        Intent intentMM10 = new Intent(this, BackGround0Activity.class);
+
+        if (make1 == 1){
+            intentMM1.putExtra("M1", 1);
+            startActivity(intentMM1);
+        } else if (make2 == 2){
+            intentMM2.putExtra("M2", 2);
+            startActivity(intentMM2);
+        } else if (make3 == 3){
+            intentMM3.putExtra("M3", 3);
+            startActivity(intentMM3);
+        } else if (make4 == 4){
+            intentMM4.putExtra("M4", 4);
+            startActivity(intentMM4);
+        } else if (make5 == 5){
+            intentMM5.putExtra("M5", 5);
+            startActivity(intentMM5);
+        } else if (make6 == 6){
+            intentMM6.putExtra("M6", 6);
+            startActivity(intentMM6);
+        } else if (make7 == 7){
+            intentMM7.putExtra("M7", 7);
+            startActivity(intentMM7);
+        } else if (make8 == 8){
+            intentMM8.putExtra("M8", 8);
+            startActivity(intentMM8);
+        } else if (make9 == 9){
+            intentMM9.putExtra("M9", 9);
+            startActivity(intentMM2);
+        } else if (make10 == 10) {
+            intentMM10.putExtra("M10", 10);
+            startActivity(intentMM10);
+        }
+    }
+}
